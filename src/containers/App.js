c95ec5ce856c9830e9abddff26bd65dba81f5524
Lucille Ablett <lucilleablett@gmail.com>
2020-01-30 10:27:39 +0000

    updated package.json and removed  title hover effect

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
@@ -34,10 +35,12 @@ class App extends Component {
         } else {
             return (
                 <div className='tc'>
-                    <h1 className='f1 grow'>RoboFriends</h1>
+                    <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
